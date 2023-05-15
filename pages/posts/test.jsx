bb1cfcbe43130f860371e635eec40c1d19ba811d
Miles-Collins <miles.collins@ymail.com>
2023-05-15 17:08:42 -0600

    NextBlog

---
+++ pages/posts/test.jsx
@@ -0,0 +1,12 @@
+
+export default function test() {
+  return (
+    <>
+    <p class="text-sm">The quick brown fox ...</p>
+    <p class="text-base">The quick brown fox ...</p>
+    <p class="text-lg">The quick brown fox ...</p>
+    <p class="text-xl">The quick brown fox ...</p>
+    <p class="text-2xl">The quick brown fox ...</p>
+  </>
+  )
+}
