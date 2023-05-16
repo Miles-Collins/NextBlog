53f9feffc5ae72b257c0edcd2d58d8f9e3c782a6
Miles-Collins <miles.collins@ymail.com>
2023-05-16 13:56:02 -0600

    sync

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,37 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>I'm a web developer with a strong background in JavaScript, C#, Node.js, .NET, Vue.js, React, Nuxt.js, and Next.js. My expertise lies in creating visually appealing and functional websites and applications. I stay updated with the latest technologies and best practices to ensure I deliver scalable and efficient solutions. With a keen eye for detail and strong problem-solving skills, I bring a collaborative approach to every project I work on. I'm passionate about creating engaging web experiences and thrive in the dynamic world of web development.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+         {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
